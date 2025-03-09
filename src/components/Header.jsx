0da450d2c91a719338e260a8fa027132e99f78ef
Lucia <luciaandreeaursu@yahoo.com>
2025-03-09 11:54:54 +0200

    basic react project with some css styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+import picture from '../assets/invest-picture.jpg'
+
+export default function Header(){
+    return <header>
+        <img src={picture} alt="" />
+        <h1>Investment Calculator</h1>
+    </header>
+}
