2651dcd83758b0edf063e988df19eac28c3ce0f1
Lucia <luciaandreeaursu@yahoo.com>
2025-03-09 11:59:54 +0200

    add a component for displaying the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 import picture from '../assets/invest-picture.jpg'
 
 export default function Header(){
-    return <header>
+    return <header id="header">
         <img src={picture} alt="" />
         <h1>Investment Calculator</h1>
     </header>
